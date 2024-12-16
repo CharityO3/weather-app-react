d89ab00584ccf849d2d82168d4b7b80c5b0bfcf6
Charity Orhoridiohwo <79048698+CharityO3@users.noreply.github.com>
2024-12-16 18:49:50 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Weather from './Weather.js';
+import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
 
 
 export default function App() {
   return (
     <div className="App">
-      <Weather city="Poznan"/>
+      <div className="container">
+        <Weather city="Poznan"/>
+        <button classsName ="btn btn-primary">Hello</button>
+      </div> 
     </div>
   );
 }
